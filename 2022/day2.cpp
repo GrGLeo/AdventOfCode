87adfe863b62775ed792d4f1d46bae1025942e14
GrGLeo <graziani.gothe.leo@gmail.com>
2025-02-15 15:40:19 +0100

    day2 part2

--- 2022/day2.cpp
+++ 2022/day2.cpp
@@ -27,52 +27,24 @@ int main() {
     return 1;
   }
 
-  int score{};
-  std::map<std::string, std::string> mapItem;
-  mapItem["A"] = "rock";
-  mapItem["B"] = "paper";
-  mapItem["C"] = "scissor";
-  mapItem["X"] = "rock";
-  mapItem["Y"] = "paper";
-  mapItem["Z"] = "scissor";
-
-  std::map<std::string, int> mapScore;
-  mapScore["rock"] = 1;
-  mapScore["paper"] = 2;
-  mapScore["scissor"] = 3;
+  std::map<std::string, std::map<std::string, int>> score;
+  score["A"]["X"] = 3; 
+  score["A"]["Y"] = 4; 
+  score["A"]["Z"] = 8; 
+  score["B"]["X"] = 1; 
+  score["B"]["Y"] = 5; 
+  score["B"]["Z"] = 9; 
+  score["C"]["X"] = 2; 
+  score["C"]["Y"] = 6; 
+  score["C"]["Z"] = 7; 
 
   std::string line;
+  int totalScore{};
   int idx{};
   while (std::getline(inputFile, line)) {
     std::vector<std::string> elems = split(line, ' ');
-    std::string pOne = mapItem[elems[0]];
-    std::string pTwo = mapItem[elems[1]];
-    idx++;
-    std::cout << std::to_string(idx) << "\n";
-    if (pOne == pTwo) {
-      score += mapScore[pTwo] + 3;
-    } else {
-      if (pOne == "rock") {
-        if (pTwo == "paper") {
-          score += 8;
-        } else if (pTwo == "scissor") {
-          score += 3;
-        }
-      } else if (pOne == "paper") {
-        if (pTwo == "rock") {
-          score += 1;
-        } else if (pTwo == "scissor") {
-          score += 9;
-        }
-      } else {
-        if (pTwo == "rock") {
-          score += 7;
-        } else if (pTwo == "paper") {
-          score += 2;
-        }
-      }
-    }
+    totalScore += score[elems[0]][elems[1]];
   }
-  std::cout << "Final score: " << std::to_string(score);
+  std::cout << "Final score: " << std::to_string(totalScore);
   return 0;
 }
